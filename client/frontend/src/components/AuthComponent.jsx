af11f568170444312c8a4f978a52943665bf1c64
Angel Lopez Pol <angellopezpol1254@gmail.com>
2023-01-27 18:55:58 -0500

    Some AuthComponent Boilerplate

---
+++ client/frontend/src/components/AuthComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * @function AuthComponent builds the UI component needed for authentication : - )
+ * @note Partially based on the video Emma provided and Norton Veto'd, going to separate out
+ * async/fetch logic and component logic as much as possible. 
+ * @returns 
+ */
+const AuthComponent = () => {
+  return (
+    <div>AuthComponent</div>
+  )
+}
+
+export default AuthComponent
