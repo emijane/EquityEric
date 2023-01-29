f4269906a08469067dcbdff7847f73fbcc673b24
Emma <88465944+emijane@users.noreply.github.com>
2023-01-28 22:55:21 -0500

    added chat functionality

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -1,24 +1,10 @@
 import './App.css';
 import Login from './pages/Login'
 import Chat from './pages/Chat'
-import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-const socket = io.connect('http://localhost:3001');
-
 function App() {
-  const [message, setMessage] = useState('');
-  const [messageReceived, setMessageReceived] = useState('s');
-  const sendMessage = () => {
-    socket.emit('send_message', { message });
-  };
-
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-    });
-  }, [socket]);
 
   return (
     <>
@@ -27,17 +13,6 @@ function App() {
         <Route path='/chat' element={<Chat/>} />
       </Routes>
     </>
-    
-    /*
-    <div className="App">
-      <input placeholder="Message..." onChange={(event) => {
-        setMessage(event.target.value);
-      }}/>
-      <button onClick={sendMessage}>Send Message</button>
-      <h1>Message:</h1>
-      {messageReceived}
-    </div>
-    */
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const { Server } = require('socket.io');
 const cors = require('cors');
 
 app.use(cors());
